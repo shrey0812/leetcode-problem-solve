938738f5fc1584a6c3eb1278215ba827c2820c25
Shrey Gupta <shreygupta0812@gmail.com>
2025-06-15 10:29:19 +0530

    Create IntergerToRoman.java

---
+++ Array/IntergerToRoman.java
@@ -0,0 +1,25 @@
+/*Roman numerals are formed by appending the conversions of decimal place values from highest to lowest. 
+Converting a decimal place value into a Roman numeral has the following rules:
+If the value does not start with 4 or 9, select the symbol of the maximal value that can be subtracted from the input, append that symbol to the result, subtract its value, and convert the remainder to a Roman numeral.
+If the value starts with 4 or 9 use the subtractive form representing one symbol subtracted from the following symbol, for example, 4 is 1 (I) less than 5 (V): IV and 9 is 1 (I) less than 10 (X): IX. 
+Only the following subtractive forms are used: 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD) and 900 (CM).
+Only powers of 10 (I, X, C, M) can be appended consecutively at most 3 times to represent multiples of 10. 
+You cannot append 5 (V), 50 (L), or 500 (D) multiple times. If you need to append a symbol 4 times use the subtractive form.
+Given an integer, convert it to a Roman numeral.*/
+
+class Solution {
+    public String intToRoman(int num) {
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+        StringBuilder result = new StringBuilder();
+
+        for(int i =0;i<values.length && num>0;i++){
+            while(num>=values[i]){
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return result.toString();
+    }
+}
